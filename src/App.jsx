f206351cb4abd0bf02d70b35def332df7dbbdf22
jcristancho2 <jocristanchool@gmail.com>
2025-08-17 18:22:59 -0500

    feat: :sparkles: creacion de app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./i18n/i18n";
+import Navbar from "./components/Navbar";
+import Home from "./modules/Home";
+import Skills from "./modules/Skills";
+import Projects from "./modules/Projects";
+import Contact from "./modules/Contact";
+
+export default function App() {
+  return (
+    <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
